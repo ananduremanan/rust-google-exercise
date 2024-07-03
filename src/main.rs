5a6717d55d490df1253b38ee481dd9f0899b59af
anandhuremanan <ananduor@gmail.com>
2024-07-03 14:24:55 +0530

    fibonacci

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 fn fib(n: u32) -> u32 {
-    if n == 0 {
-        return 0;
-    } else if n == 1 {
-        return 1;
+    if n < 2 {
+        return n;
     } else {
         return fib(n - 1) + fib(n - 2);
     }
 }
 
 fn main() {
-    let n: u32 = 1;
+    let n = 20;
     println!("fib({n}) = {}", fib(n));
 }
